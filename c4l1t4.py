1ff5370c28b17a1e2772bcfd260f7c9d6fafc8c0
Roman Stelmakh <romuarste@gmail.com>
2023-06-23 21:30:13 +1100

    s_palindrome_with_loop added.
    Добавлена функция is_palindrome_with_loop, которая с помощью цикла
    проверяет, является ли строка полиндромом. Цикл проходит по числам
    от 0 до половины длины строки округлённой до меньшего целого. Если
    строка с нечётным количеством символов, то средний символ просто не
    участвует в сравнении. Каждый проход цикла сравниваются буквы с
    индексами [i] и [-i -1]. В случае, если буквы не одинаковы, функция
    сразу возвращает False, если все буквы оказались одинаковы,
    происходит выход из цикла, и функция возвращает True.

--- c4l1t4.py
+++ c4l1t4.py
@@ -1,2 +1,8 @@
 def is_palindrome(word):
     return word == word[::-1]
+
+def is_palindrome_with_loop(word):
+    for i in range(int(len(word) / 2)):
+        if word[i] != word[-i - 1]:
+            return False
+    return True
